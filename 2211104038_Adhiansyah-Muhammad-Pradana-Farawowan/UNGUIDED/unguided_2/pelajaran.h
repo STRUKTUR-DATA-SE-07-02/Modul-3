23b825cc6765b9a764bab49cfe23b2e1dc739da6
Adhiansyah Ancha <69331906+adhiansyahancha@users.noreply.github.com>
2024-10-15 20:07:16 +0700

    Update pelajaran.h

--- 2211104038_Adhiansyah-Muhammad-Pradana-Farawowan/UNGUIDED/unguided_2/pelajaran.h
+++ 2211104038_Adhiansyah-Muhammad-Pradana-Farawowan/UNGUIDED/unguided_2/pelajaran.h
@@ -1,1 +1,14 @@
+#ifndef _PELAJARAN_H
+#define _PELAJARAN_H
 
+#include <iostream>
+
+struct pelajaran {
+    std::string namaMapel;
+    std::string kodeMapel;
+};
+
+pelajaran create_pelajaran(std::string namapel, std::string kodepel);
+void tampil_pelajaran(pelajaran pel);
+
+#endif
